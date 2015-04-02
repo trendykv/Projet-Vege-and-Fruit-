b0276dc04ec63ace847f6bfd731fba72940d2c48
Kevin <kevin.raharison@isep.fr>
2015-04-02 10:55:10 +0200

    Pages pour Wamp
    Contient:
    Les deux premieres versions de page d'accueil
    La page de pré-entrée
    Le dossier contenant les images du site

---
+++ Autre _Pages/Page_1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+	
+	    <meta charset="utf-8" />
+	    <link rel="stylesheet" href="Page_1.css" />
+	    <title>Vege&Fruit</title>
+	
+	
+	</head>
+	
+	<body>
+	<script>
+	
+         var userName = prompt('Entrez votre département');
+		 alert(userName);
+	
+	
+	</script>
+	</body>
---
+++ Page_accueil..js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+	
+	    <meta charset="utf-8" />
+	    <link rel="stylesheet" href="Page_accueil.css" />
+	    <title>Vege&Fruit</title>
+	
+	
+	</head>
+	
+	<body>
+	<script>
+	
+         alert('Erreur de script');
+	
+	
+	</script>
+	</body>
